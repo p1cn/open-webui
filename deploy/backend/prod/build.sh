3e390a931eb0f17d8cdf48c4f5a09fdfd388831c
leantli <leantli@qq.com>
2025-02-14 20:18:04 +0800

    feat: change build.sh config path

--- deploy/backend/prod/build.sh
+++ deploy/backend/prod/build.sh
@@ -18,9 +18,9 @@ export https_proxy=http://proxy-inner.p1.cn:1338
 Date=$(date +%Y%m%d%H%M%S)
 TAG=${HARBOR}/${ENV}/${SERVICE_NAME}:${VERSION}-${Date}
 cd ${SOURCE_CODE}
-sed -i "s#{{service-name}}#$SERVICE_NAME#g" ${SOURCE_CODE}/deploy/${ENV}/entrypoint.sh
-sed -i "s#image:.*#image: $TAG#" ${SOURCE_CODE}/deploy/${ENV}/$SERVICE_NAME.yaml
-echo "docker build --build-arg service=${SERVICE_NAME} --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/${ENV}/Dockerfile"
-docker build --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/${ENV}/Dockerfile
+sed -i "s#{{service-name}}#$SERVICE_NAME#g" ${SOURCE_CODE}/deploy/backend/${ENV}/entrypoint.sh
+sed -i "s#image:.*#image: $TAG#" ${SOURCE_CODE}/deploy/backend/${ENV}/$SERVICE_NAME.yaml
+echo "docker build --build-arg service=${SERVICE_NAME} --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/backend/${ENV}/Dockerfile"
+docker build --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/backend/${ENV}/Dockerfile
 echo "Image: ${TAG}"
 docker push ${TAG}
