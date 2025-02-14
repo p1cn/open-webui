a3a022cc1a0adde1cae99e5e01f9fe4bdbde6804
liulei <liulei@p1.com>
2025-02-14 21:12:44 +0800

    change entrypoint.sh

--- deploy/gpt-oi-web/prod/entrypoint.sh
+++ deploy/gpt-oi-web/prod/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /app/nginx_binary/nginx -c /app/infra-lottery-web/nginx/nginx.conf -g 'daemon off;'
+exec /app/nginx_binary/nginx -c /app/gpt-oi-web/nginx/nginx.conf -g 'daemon off;'
