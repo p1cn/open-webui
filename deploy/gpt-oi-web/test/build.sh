62f94e3a3f5148cd9a1f62a2b3d1db45bfed57d7
liulei <liulei@p1.com>
2025-02-14 20:44:32 +0800

    change config

--- deploy/gpt-oi-web/test/build.sh
+++ deploy/gpt-oi-web/test/build.sh
@@ -16,9 +16,9 @@ echo "HARBOR: ${HARBOR}"
 Date=$(date +%Y%m%d%H%M%S)
 TAG=${HARBOR}/${ENV}/${SERVICE_NAME}:${VERSION}-${Date}
 cd ${SOURCE_CODE}
-sed -i "s#{{service-name}}#$SERVICE_NAME#g" ${SOURCE_CODE}/deploy/${ENV}/entrypoint.sh
-sed -i "s#image:.*#image: $TAG#" ${SOURCE_CODE}/deploy/${ENV}/$SERVICE_NAME.yaml
-echo "docker build --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/${ENV}/Dockerfile"
-docker build --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/${ENV}/Dockerfile
+sed -i "s#{{service-name}}#$SERVICE_NAME#g" ${SOURCE_CODE}/deploy/gpt-oi-web/${ENV}/entrypoint.sh
+sed -i "s#image:.*#image: $TAG#" ${SOURCE_CODE}/deploy/gpt-oi-web/${ENV}/$SERVICE_NAME.yaml
+echo "docker build --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/gpt-oi-web/${ENV}/Dockerfile"
+docker build --build-arg version=${VERSION} --build-arg env=${ENV} --build-arg harbor=${HARBOR} -t ${TAG} . -f deploy/gpt-oi-web/${ENV}/Dockerfile
 echo "Image: ${TAG}"
 docker push ${TAG}
