75f351beb3b7ed5858162ca82e13a553350dcc0e
leantli <leantli@qq.com>
2025-02-19 19:00:53 +0800

    chore: temp

--- backend/open_webui/utils/middleware.py
+++ backend/open_webui/utils/middleware.py
@@ -568,7 +568,7 @@ async def chat_completion_files_handler(
         except Exception as e:
             log.exception(e)
 
-        log.debug(f"sources: {json.dumps(sources,ensure_ascii=False)}")
+        log.info(f"sources: {json.dumps(sources,ensure_ascii=False)}")
 
     return body, {"sources": sources}
 
@@ -792,13 +792,12 @@ async def process_chat_payload(request, form_data, metadata, user, model):
     # If context is not empty, insert it into the messages
     if len(sources) > 0:
         context_string = ""
-        for source_idx, source in enumerate(sources):
-            source_id = source.get("source", {}).get("name", "")
-            log.debug(f"source_idx = {source_idx}, source_id = {source_id}")
+        source_idx = 0
+        for _, source in enumerate(sources):
             if "document" in source:
                 for doc_idx, doc_context in enumerate(source["document"]):
-                    log.debug(f"doc_idx = {doc_idx}")
-                    context_string += f"<source><source_id>{doc_idx}</source_id><source_context>{doc_context}</source_context></source>\n"
+                    context_string += f"<source><source_id>{source_idx}</source_id><source_context>{doc_context}</source_context></source>\n"
+                    source_idx += 1
 
         context_string = context_string.strip()
         prompt = get_last_user_message(form_data["messages"])
